9dfb831253c02ff9c7c1ef98c76a6c2452298238
Danil Khodos <theholycheese033@mail.ru>
2021-01-17 15:18:32 +1000

    sd
    sdsd

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+  },
+});
+
+const swiper2 = new Swiper('.swiper-container--reviews', {
+  zoom: {
+    maxRatio: 1.5,
+    minRatio: 1
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+  autoplay: {
+    delay: 5000,
+  },
+});
+
+ymaps.ready(init);
+function init(){
+    const myMap = new ymaps.Map("map", {
+        // Координаты центра карты.
+        // Порядок по умолчанию: «широта, долгота».
+        // Чтобы не определять координаты центра карты вручную,
+        // воспользуйтесь инструментом Определение координат.
+        center: [55.76, 37.64],
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 18
+    });
+}
+
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
